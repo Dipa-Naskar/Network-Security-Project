438ce26942fa4100c4662c782f43d6df450e1f0b
Dipa-Naskar <86941560+Dipa-Naskar@users.noreply.github.com>
2021-07-05 13:27:47 +0530

    Hashing  using md5 , sha256 , sha512

---
+++ hashesofstringdata.py
@@ -0,0 +1,11 @@
+import hashlib
+str = input("Enter any string :")
+h1=hashlib.md5(str.encode())
+h2=hashlib.sha256(str.encode())
+h3=hashlib.sha512(str.encode())
+final1=(h1.hexdigest())
+final2=(h2.hexdigest())
+final3=(h3.hexdigest())
+print("The converted hash of your string using md5:" +final1)
+print("The converted hash of your string using sha256 :" +final2)
+print("The converted hash of your string using sha512 :" +final3)
